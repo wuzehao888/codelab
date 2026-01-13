327e1f9fcecebfe473292637c3a8d71f349c07d6
wuzehao888 <wzh719691343@163.com>
2026-01-13 23:49:20 +0800

    add DiceRoller practise

---
+++ DiceRoller/settings.gradle.kts
@@ -0,0 +1,29 @@
+pluginManagement {
+    repositories {
+        maven(url = uri("https://maven.aliyun.com/repository/central"))
+        maven(url = uri("https://maven.aliyun.com/repository/google"))
+        maven(url = uri("https://maven.aliyun.com/repository/public"))
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        maven(url = uri("https://maven.aliyun.com/repository/central"))
+        maven(url = uri("https://maven.aliyun.com/repository/google"))
+        maven(url = uri("https://maven.aliyun.com/repository/public"))
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "DiceRoller"
+include(":app")
